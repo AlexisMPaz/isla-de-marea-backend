fa07181fd0ff44985bf9dc3bb953674b9137b6ff
Alexis Paz <alexismpaz@gmail.com>
2023-06-29 19:31:37 -0300

    Test borrados

--- src/routes/mocking.routes.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { mockingProducts } from "../controllers/mocking.controllers.js";
-
-export const routerMocking = Router();
-
-routerMocking.get('/mockingproducts', mockingProducts);
