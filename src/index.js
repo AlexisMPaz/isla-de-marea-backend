3f44374a866e3473dd712e95354db02c5afea41c
Alexis Paz <alexismpaz@gmail.com>
2023-07-21 00:41:34 -0300

    TEST CORS 3

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ const app = express();
 const whiteList = ["http://localhost:3000", "http://localhost:8080", "https://app-isla-de-marea.onrender.com"];
 const corsOptions = {
   origin: function (origin, callback) {
-    if (whiteList.indexOf(origin) !== -1 || !origin) {
+    if (whiteList.indexOf(origin) == -1 || !origin) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
